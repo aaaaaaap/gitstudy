9902f190f7ae3eb4764c484c7fb7e36f27328094
aaaaaaap <aaaaaaap@paran.com>
2017-04-21 21:26:33 +0900

    Hello world file add

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+"""
+Author: Jinwon Kang
+Date : 2016-07-13
+이 프로그램은 Hello world 를 출력하는 프로그램이다
+"""
+
+# hello.py
+print("Hello world!!!")
+print("Hello world!!!")
+print("Hello world!!!")
+print("Hello world!!!")
+print("Hello world!!!")
+print("Hello world!!!")
+
+
